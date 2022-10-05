d0e20c1be4319ad72cfaf5f3f5374c28ddcd99cf
amhml2001 <67562505+amhml2001@users.noreply.github.com>
2022-10-05 12:13:17 -0300

    Add files via upload

---
+++ TwoFer.groovy
@@ -0,0 +1,10 @@
+class TwoFer {
+    static String twoFer(String name) {
+        if (name){
+            "One for " + name + ", one for me."
+        }
+        else{
+            "One for you, one for me."
+        }
+    }
+}
